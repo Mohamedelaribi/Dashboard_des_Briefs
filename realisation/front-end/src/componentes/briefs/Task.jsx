a535e7f7ff82babc0dd8a915f9bcd9fb5261558d
Mohamedelaribi <mohamed.elaribi.solicode@gmail.com>
2022-12-16 02:02:15 +0100

    push

--- realisation/front-end/src/componentes/briefs/Task.jsx
+++ realisation/front-end/src/componentes/briefs/Task.jsx
@@ -3,24 +3,47 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import './style/briefs.css';
 
 
 
 
 function Task() {
 
-  const[Task ,setTask] = useState([])
+  const[Tasks ,setTask] = useState([])
   const{id}=useParams();
-  console.log(id)
   useEffect(()=>{
     axios.get(`http://127.0.0.1:8000/api/brief/${id}/tasks`).then((response)=>{
       console.log(response.data)
+      setTask(response.data)
     })
   },[])
 
 
   return (
-    <div className=''></div>
+    <div className='taskContainer'>
+      <h2 className='nameBrief'>{Tasks.nameBrief}</h2>
+      <div className="taskList">
+        {Tasks.map(task=>(
+          <div className="cardTask">
+              <h2 className='titleTask'>{task.nameTask}</h2>
+            <div className="taskNameContainer">
+              <h3 className="nameOfTask">Nom De Tache:</h3>
+              <h3 className="taskName"></h3>
+            </div>
+            <div className="StartDateContainer">
+              <p className="startDateOfTask">Date/Heure de debut:</p>
+              <p className="taskStartDate">11/01/2022 10:00</p>
+            </div>
+            <div className="endDateContainer">
+              <p className="endDateOfTask">Date/Heure de debut:</p>
+              <p className="taskEndtDate">11/01/2022 10:00</p>
+            </div>
+        </div>
+        ))}
+  
+      </div>
+    </div>
   )
 }
 
